fd261e69b0960d58d1c950a4adee07c7ef1246b6
Mohammad Fallah <fallahmohammadm@gmail.com>
2023-05-06 14:15:23 +0330

    connect to db and test req/response

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,42 @@
+const db = require("../models");
+const User = db.user;
+
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+
+    const user = new User({
+        title: req.body.title,
+    });
+    user
+        .save(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Tutorial."
+            });
+        });
+};
+
+
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+
+    User.find(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
+};
--- server.js
+++ server.js
@@ -22,6 +22,24 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+require("./app/routes/user.routes.js")(app);
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
