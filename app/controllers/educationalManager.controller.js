3b2979c464a65d2b20b70b6432045dff4f6dd350
Mohammad Fallah <fallahmohammadm@gmail.com>
2023-05-11 10:00:15 +0330

    add manager controller

---
+++ app/controllers/educationalManager.controller.js
@@ -0,0 +1,107 @@
+const db = require("../models");
+const EducationalManager = db.educationalManager;
+
+exports.create = (req, res) => {
+
+    if (!req.body.department && !req.body.user_id && !req.body.password && !req.body.email && !req.body.phone_no) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+
+    const educationalManager = new EducationalManager({
+        department : req.body.department,
+        name : req.body.name,
+        user_id : req.body.user_id,
+        password : req.body.password,
+        email : req.body.email,
+        phone_no : req.body.phone_no
+    });
+    educationalManager
+        .save(educationalManager)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating document."
+            });
+        });
+};
+
+
+exports.findAll = (req, res) => {
+
+    EducationalManager.find()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving documents."
+            });
+        });
+};
+
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    EducationalManager.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found document with id " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving document with id=" + id });
+        });
+};
+
+exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+
+    const id = req.params.id;
+
+    EducationalManager.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update document with id=${id}. Maybe document was not found!`
+                });
+            } else res.send({ message: "document was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating document with id=" + id
+            });
+        });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    EducationalManager.findByIdAndRemove(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete document with id=${id}. Maybe document was not found!`
+                });
+            } else {
+                res.send({
+                    message: "document was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete document with id=" + id
+            });
+        });
+};
