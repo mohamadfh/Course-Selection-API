cd23f3fe4174740085bbd18f6b650d42d55ccf89
Mohammad Fallah <fallahmohammadm@gmail.com>
2023-05-11 08:54:44 +0330

    add professor controller

---
+++ app/controllers/professor.controller.js
@@ -0,0 +1,110 @@
+const db = require("../models");
+const Professor = db.professor;
+
+exports.create = (req, res) => {
+
+    if (!req.body.department && !req.body.major && !req.body.rank && !req.body.name
+        && !req.body.user_id && !req.body.password && !req.body.email && !req.body.phone_no) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+
+    const professor = new Professor({
+        department : req.body.department,
+    major : req.body.major,
+    rank : req.body.rank,
+    name : req.body.name,
+    user_id : req.body.user_id,
+    password : req.body.password,
+    email : req.body.email,
+        phone_no : req.body.phone_no
+    });
+    professor
+        .save(professor)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating document."
+            });
+        });
+};
+
+
+exports.findAll = (req, res) => {
+
+    Professor.find()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving documents."
+            });
+        });
+};
+
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    Professor.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found document with id " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving document with id=" + id });
+        });
+};
+
+exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+
+    const id = req.params.id;
+
+    Professor.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update document with id=${id}. Maybe document was not found!`
+                });
+            } else res.send({ message: "document was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating document with id=" + id
+            });
+        });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Professor.findByIdAndRemove(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete document with id=${id}. Maybe document was not found!`
+                });
+            } else {
+                res.send({
+                    message: "document was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete document with id=" + id
+            });
+        });
+};
