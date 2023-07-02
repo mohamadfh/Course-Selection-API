140c88cc429eeb1fd03f96765cd893568f4ff9a3
HosainP <hosain.pishgahi@gmail.com>
2023-07-02 14:04:37 +0330

    semesters list page added for professor

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
+import ProfSems from "./pages/professor/semesters";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/prof-sems" element={<ProfSems />} />
+
         {/* Add more routes here */}
       </Routes>
     </Router>
